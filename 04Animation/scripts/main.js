1d5b9feffe235a0435e943e31ce36a69337d1814
Julien Heesterman <18098843@student.hhs.nl>
2020-05-12 11:37:09 +0200

    Update main.js

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -8,7 +8,6 @@
  	myOtherBox.object3D.rotation.x -= rotationSpeed;
  	myOtherBox.object3D.rotation.y += rotationSpeed;
  	myOtherBox.object3D.rotation.z -= rotationSpeed;
-console.log(myOtherBox.object3D.rotation.x);
 }
 
 setInterval(spin, 16); // equivalent to 60 fps
